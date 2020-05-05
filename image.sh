5c18bcdf859f18d93c68bef053f74d0e2cdcbeea
pengfei.lv <pengfei.lv@ucloud.cn>
2020-05-05 21:29:54 +0800

    d

--- image.sh
+++ image.sh
@@ -5,13 +5,13 @@ echo "当前环境"-$e
 app_name=atlantis
 version_num=$e
 
-default_registry=https://hub.docker.com/repository/docker/lvpf/golang-project
-default_project=atlantis
+default_registry=https://hub.docker.com/repository/docker
+default_project=/lvpf/golang-project
 default_user=lpengfei66@163.com
 default_pwd=lpf12345@
 
 if [ "$e" != pre ]; then
-	default_registry=https://hub.docker.com/repository/docker/lvpf/golang-project
+	default_registry=https://hub.docker.com/repository/docker
 	default_user=lvpf
 fi
 
@@ -19,9 +19,10 @@ echo $default_registry
 
 echo $(date "+%Y-%m-%d %H:%M:%S")-开始制作镜像
 docker build --rm -t $app_name:$version_num .
-echo $(data "+%Y-%m-%d %H:%M:%S")-镜像制作完成
+echo $(date "+%Y-%m-%d %H:%M:%S")-镜像制作完成
 
 echo $(date "+%Y-%m-%d %H:%M:%S")-开始打tag
+echo $app_name:$version_num
 docker tag $app_name:$version_num $default_registry/$default_project/$app_name\:$version_num
 echo $(date "+%Y-%m-%d %H:%M:%S")-打tag完成
 
